c8ce48bfe663520da842eae0bbdea38675d029d5
shonayevshyngys <shonayevshyngys@gmail.com>
2023-08-27 18:56:02 +0200

    Initial commit

---
+++ src/main/java/com/github/fabrickapp/FabrickAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabrickapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrickAppApplication.class, args);
+	}
+
+}
