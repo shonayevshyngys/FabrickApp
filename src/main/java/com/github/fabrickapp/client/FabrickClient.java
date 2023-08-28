187d28f2fa5acda702025e90be16329988eb1a79
shonayevshyngys <shonayevshyngys@gmail.com>
2023-08-28 21:48:38 +0200

    Client enhancement. FabrickClient now uses values from application.properties.

--- src/main/java/com/github/fabrickapp/client/FabrickClient.java
+++ src/main/java/com/github/fabrickapp/client/FabrickClient.java
@@ -9,19 +9,21 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class FabrickClient {
+    private final WebClient client;
 
-    @Value("apikey")
-    private String apiKey;
-    private final WebClient client = WebClient.builder()
-            .baseUrl("https://sandbox.platfr.io")
-            .defaultHeader("Auth-Schema", "S2S")
-            .defaultHeader("apikey", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP")
-            .build();
+    public FabrickClient(@Value("${apikey}") String apiKey, @Value("${url}") String url) {
+        client = WebClient.builder()
+                .baseUrl(url)
+                .defaultHeader("Auth-Schema", "S2S")
+                .defaultHeader("apikey", apiKey)
+                .build();
+    }
 
     public void getBalance()
     {
         var res = client.get()
                 .uri("/api/gbs/banking/v4.0/accounts")
                 .exchangeToMono(clientResponse -> clientResponse.bodyToMono(String.class)).block();
+        System.err.println(res);
     }
 }
