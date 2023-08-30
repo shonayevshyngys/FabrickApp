32f35f67dac884a24d9fcd4158d860fbcf3ca62f
shonayevshyngys <shonayevshyngys@gmail.com>
2023-08-30 15:12:50 +0200

    Lombok loggings - added some structured loggings to application

--- src/main/java/com/github/fabrickapp/client/FabrickClient.java
+++ src/main/java/com/github/fabrickapp/client/FabrickClient.java
@@ -1,3 +1,4 @@
+
 package com.github.fabrickapp.client;
 
 import com.github.fabrickapp.dtos.fabrickdtos.AccountDTO;
@@ -5,6 +6,9 @@ import com.github.fabrickapp.dtos.fabrickdtos.BalanceDTO;
 import com.github.fabrickapp.dtos.fabrickdtos.MoneyTransferDTO;
 import com.github.fabrickapp.dtos.fabrickdtos.MoneyTransferRequestDTO;
 import com.github.fabrickapp.dtos.fabrickdtos.TransactionsDTO;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -14,9 +18,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Service
+@Log4j2
 public class FabrickClient {
     private final WebClient client;
     private final long accountId;
+
+    private static final Marker CLIENT_MARKER = MarkerManager.getMarker("Fabrick_Client");
 
     public FabrickClient(@Value("${apikey}") String apiKey, @Value("${url}") String url, @Value("${accountId}") String id) {
         accountId = Long.parseLong(id);
@@ -30,6 +37,7 @@ public class FabrickClient {
 
     public BalanceDTO getBalance()
     {
+        log.info(CLIENT_MARKER,"{} BALANCE REQUEST", accountId);
         return client.get()
                 .uri("/api/gbs/banking/v4.0/accounts/%d/balance".formatted(accountId))
                 .exchangeToMono(clientResponse -> clientResponse.bodyToMono(BalanceDTO.class)).block();
@@ -37,6 +45,7 @@ public class FabrickClient {
 
     public AccountDTO getAccount()
     {
+        log.info(CLIENT_MARKER, "{} ACCOUNT REQUEST", accountId);
         return client.get()
                 .uri("/api/gbs/banking/v4.0/accounts/%d".formatted(accountId))
                 .exchangeToMono(clientResponse -> clientResponse.bodyToMono(AccountDTO.class)).block();
@@ -44,6 +53,7 @@ public class FabrickClient {
 
     public TransactionsDTO getTransactions(LocalDate from, LocalDate to)
     {
+        log.info(CLIENT_MARKER, "{} TRANSACTIONS REQUEST", accountId);
         var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return client.get()
                 .uri("/api/gbs/banking/v4.0/accounts/%d/transactions?fromAccountingDate=%s&toAccountingDate=%s"
@@ -53,6 +63,7 @@ public class FabrickClient {
 
     public MoneyTransferDTO createMoneyTransfer(MoneyTransferRequestDTO requestDTO)
     {
+        log.info(CLIENT_MARKER, "{} POST MONEY TRANSFER REQUEST", accountId);
         return client.post()
                 .uri("/api/gbs/banking/v4.0/accounts/%d/payments/money-transfers".formatted(accountId))
                 .bodyValue(requestDTO)
